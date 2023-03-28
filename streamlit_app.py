f1365a219ab456ff984ddddb17dfe3d7e72fb451
navaratveeraforwork <129141376+navaratveeraforwork@users.noreply.github.com>
2023-03-28 14:38:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # streamlit_app.py
 import streamlit as st
+import streamlit_authenticator as stauth
 import pandas as pd
 from google.oauth2 import service_account
 from gsheetsdb import connect
@@ -12,12 +13,29 @@ credentials = service_account.Credentials.from_service_account_info(
     ],
 )
 
+# Authentication
+authenticator = stauth.Authenticate(
+    {
+        "usernames": {
+            st.secrets["login_user"]: {
+                "email": "test@test.com",
+                "name": st.secrets["login_display_name"],
+                "password": stauth.Hasher([st.secrets["login_password"]]).generate()[0],
+            }
+        }
+    },
+    "jk_lookup",
+    "jk_lookup",
+    30,
+)
+
 conn = connect(credentials=credentials)
+
 
 @st.cache_data(ttl=600)
 def get_dataframe():
     rows = conn.execute(
-            f'''SELECT 
+        f"""SELECT 
                     "รหัส" AS STRING,
                     "ชื่อสินค้า" AS STRING,
                     "หมายเหตุ/อื่นๆ" AS STRING,
@@ -28,8 +46,9 @@ def get_dataframe():
                     "วันที่อัพเดท" AS STRING,
                     "สถานะ" AS STRING
                 FROM "{st.secrets["private_gsheets_url"]}"
-                ''',
-            headers=1)
+                """,
+        headers=1,
+    )
     rows = rows.fetchall()
 
     df = pd.DataFrame(
@@ -44,9 +63,10 @@ def get_dataframe():
             "ภาษี",
             "วันที่อัพเดท",
             "สถานะ",
-        ])
+        ],
+    )
     return df
-    
+
 
 def filter_dataframe():
     """
@@ -67,9 +87,19 @@ def filter_dataframe():
         user_text_input = right.text_input(
             f"Substring or regex in {column}",
         )
-        
+
         df = get_dataframe()
 
     return df[df["ชื่อสินค้า"].str.contains(user_text_input, regex=False)]
 
-st.dataframe(filter_dataframe(), use_container_width=True)
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.dataframe(filter_dataframe(), use_container_width=True)
+elif authentication_status is False:
+    st.error('Username/password is incorrect')
+elif authentication_status is None:
+    st.warning('Please enter your username and password')
+
